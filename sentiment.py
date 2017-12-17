cd82194c4cd99d6733cd6d61781be321b5ad5bb4
Yehia Said <yehiahesham@aucegypt.edu>
2017-12-17 21:31:20 +0200

    tokenizer approach

--- sentiment.py
+++ sentiment.py
@@ -1,22 +1,24 @@
-from keras.models import Model
 from keras.optimizers import SGD, Adam, Nadam, RMSprop
-from keras.models import Sequential
+from keras.models import Sequential,Model,load_model
 from keras.layers.core import Dense, Activation,Dropout ,Flatten
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator
-from keras import regularizers
+from keras.preprocessing import sequence
+from keras.preprocessing.text import text_to_word_sequence,one_hot,Tokenizer
 from keras.constraints import maxnorm
-from keras.models import load_model
 from keras.callbacks import ModelCheckpoint,TensorBoard, ReduceLROnPlateau
 from keras.applications import Xception
-import keras
+from keras import regularizers
 from keras import backend as K
+import keras
 import numpy as np
+import pandas as pd
 import cv2
 import os
 import glob
 import math
-import pandas as pd
+seed = 7
+np.random.seed(seed)
 
 def load_TrainingData(path):     #loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
     D = pd.read_csv(path, sep='\t', header=0)
@@ -30,10 +32,12 @@ def load_TestingData(path):     #loads data , caluclate Mean & subtract it data,
     X_test=D.ix[:,:3]
     return  X_test
 
+def myfunction( x ):
+    return sum(x)
 
 X_train, Y_train, feature_names = load_TrainingData('./train.tsv')
 X_test = load_TestingData('./test.tsv')
-
+print '============================== Training data shapes =============================='
 print 'X_train.shape is ', X_train.shape
 print 'Y_train.shape is ',Y_train.shape
 
@@ -48,34 +52,81 @@ X_Val = X_train[:num_test]
 X_train = X_train[num_test:]
 Y_train = Y_train[num_test:]
 
+# X_train_words=[]
+#
+# # for i in xrang(num_test+1,X_train.shape[0])
+# for i in xrange(num_test+1,10):
+    # X_train_words = X_train_words | set(text_to_word_sequence(X_train[i]))
 
 
-# print 'Featu  re are ',feature_names
+#
+# encoded = one_hot(X_train, round(len(X_train_words)*1.3))
+# print encoded
+
+maxWordCount= 37
+maxDictionary_size=5000
+
+# create the tokenizer
+train_Tokenizer = Tokenizer()
+# fit the tokenizer on the documents
+train_Tokenizer.fit_on_texts(X_train)
+train_Tokenizer_vocab_size = len(train_Tokenizer.word_index) + 1
+
+encoded_words = train_Tokenizer.texts_to_sequences(X_train)
+
+
+#padding all text to same size
+encodedPadded_words = sequence.pad_sequences(encoded_words, maxlen=maxWordCount)
+# X_Val = sequence.pad_sequences(X_Val, maxlen=maxWordCount)
+# X_test = sequence.pad_sequences(X_test, maxlen=maxWordCount)
+
+print encodedPadded_words[0]
+print 'Featu  re are ',feature_names
+print '============================== After extracting a validation set of '+ str(num_test)+' ============================== '
+print '============================== Training data shapes =============================='
 print 'X_train.shape is ', X_train.shape
-print 'X_Val.shape is ', X_Val.shape
 print 'Y_train.shape is ',Y_train.shape
+print '============================== Validation data shapes =============================='
 print 'Y_Val.shape is ',Y_Val.shape
-print '=============================='
+print 'X_Val.shape is ', X_Val.shape
+print '============================== Test data shape =============================='
 print 'X_test.shape is ', X_test.shape
 
-model = Sequential()
-model.add(Flatten(input_shape=X_train.shape[0:]))
-model.add(Dense(32))
 
+learning_rate=0.0001
+epochs = 50
+batch_size = 128 #32
+sgd = SGD(lr=learning_rate, nesterov=True, momentum=0.7, decay=1e-4)
+
+
+
+
+print '============================== After padding all text to same size of '+ str(maxWordCount)+' =============================='
+print '============================== Training data shapes =============================='
+print 'X_train.shape is ', X_train.shape
+print 'Y_train.shape is ',Y_train.shape
+print '============================== Validation data shapes =============================='
+print 'Y_Val.shape is ',Y_Val.shape
+print 'X_Val.shape is ', X_Val.shape
+print '============================== Test data shape =============================='
+print 'X_test.shape is ', X_test.shape
+
+#model
+model = Sequential()
+
+model.add(Embedding(maxDictionary_size, 32, input_length=maxWordCount)) #to change words to ints
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
  #hidden layers
 # model.add(Dropout(0.5))
-
-model.add(Dense(16, activation='relu'))
-# model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dropout(0.5))
 
  #output layer
 model.add(Dense(5, activation='softmax'))
 
 # Compile model
-learning_rate=0.0001
-epochs = 50
-batch_size = 128 #32
-sgd = SGD(lr=learning_rate, nesterov=True, momentum=0.7, decay=1e-4)
 # adam=Adam(lr=learning_rate, beta_1=0.7, beta_2=0.999, epsilon=1e-08, decay=0.0000001)
 
 model.summary()
@@ -125,4 +176,5 @@ history  = model.fit(X_train, Y_train, nb_epoch = epochs, batch_size=batch_size,
 
 print ("=============================== Predicting =========================================")
 
-# score = model.evaluate(X_test, y_test, verbose=0)
+score = model.evaluate(X_Val, Y_Val, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))

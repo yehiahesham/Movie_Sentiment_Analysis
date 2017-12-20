9600a843292aa328e26ff54588b7fc32a9b9b954
Yehia Said <yehiahesham@aucegypt.edu>
2017-12-21 00:19:34 +0200

    problem was on the loss. it was binary cross entr.  while it should be categorical cross entr.got with LSTM 63.27 accuray

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,6 @@
 from keras.optimizers import SGD, Adam, Nadam, RMSprop
 from keras.models import Sequential,Model,load_model
-from keras.layers import Embedding,Conv1D
+from keras.layers import Embedding,Conv1D,MaxPooling1D
 from keras.layers.core import Dense, Activation,Dropout ,Flatten
 from keras.layers.recurrent import LSTM
 from keras.utils import np_utils
@@ -8,7 +8,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import sequence
 from keras.preprocessing.text import text_to_word_sequence,one_hot,Tokenizer
 from keras.constraints import maxnorm
-from keras.callbacks import ModelCheckpoint,TensorBoard, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint,TensorBoard, ReduceLROnPlateau,EarlyStopping
 from keras.applications import Xception
 from keras import regularizers
 from keras import backend as K
@@ -25,8 +25,8 @@ np.random.seed(seed)
 def load_TrainingData(path):     #loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
     D = pd.read_csv(path, sep='\t', header=0)
     feature_names  = np.array(list(D.columns.values))
-    Y_train = np.array(list(D['Sentiment']));
     X_train = np.array(list(D['Phrase']))
+    Y_train = np.array(list(D['Sentiment']));
     return  X_train, Y_train, feature_names
 
 def load_TestingData(path):     #loads data , caluclate Mean & subtract it data, gets the COV. Matrix.
@@ -34,6 +34,12 @@ def load_TestingData(path):     #loads data , caluclate Mean & subtract it data,
     X_test=np.array(list(D['Phrase']))
     X_test_PhraseID=np.array(list(D['PhraseId']))
     return  X_test,X_test_PhraseID
+
+def shuffle_2(a, b): # Shuffles 2 arrays with the same order
+    s = np.arange(a.shape[0])
+    np.random.shuffle(s)
+    return a[s], b[s]
+
 
 
 X_train, Y_train, feature_names = load_TrainingData('./train.tsv')
@@ -45,14 +51,16 @@ print 'Y_train.shape is ',Y_train.shape
 
 Tokenizer = Tokenizer()
 Tokenizer.fit_on_texts(np.concatenate((X_train, X_test), axis=0))
+# Tokenizer.fit_on_texts(X_train)
 Tokenizer_vocab_size = len(Tokenizer.word_index) + 1
 print "Vocab size",Tokenizer_vocab_size
 
 #masking
-num_test = 10000
+num_test = 32000
 mask = range(num_test)
 
 Y_Val = Y_train[:num_test]
+Y_Val2 = Y_train[:num_test]
 X_Val = X_train[:num_test]
 
 
@@ -76,12 +84,13 @@ X_Train_encodedPadded_words = sequence.pad_sequences(encoded_words, maxlen=maxWo
 X_Val_encodedPadded_words = sequence.pad_sequences(encoded_words2, maxlen=maxWordCount)
 X_test_encodedPadded_words = sequence.pad_sequences(encoded_words3, maxlen=maxWordCount)
 
-
-
 # One Hot Encoding
 Y_train = keras.utils.to_categorical(Y_train, 5)
 Y_Val   = keras.utils.to_categorical(Y_Val, 5)
 
+
+#shuffling the traing Set
+shuffle_2(X_Train_encodedPadded_words,Y_train)
 
 print 'Featu are ',feature_names
 print '============================== After extracting a validation set of '+ str(num_test)+' ============================== '
@@ -112,15 +121,20 @@ print 'X_test.shape is ', X_test.shape
 model = Sequential()
 
 model.add(Embedding(maxDictionary_size, 32, input_length=maxWordCount)) #to change words to ints
-# model.add(LSTM(5))
 # model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 # model.add(MaxPooling1D(pool_size=2))
- #hidden layers
 # model.add(Dropout(0.5))
-model.add(Flatten())
-model.add(Dense(250, activation='relu',W_constraint=maxnorm(1)))
-model.add(Dropout(0.5))
+# model.add(Conv1D(filters=32, kernel_size=2, padding='same', activation='relu'))
+# model.add(MaxPooling1D(pool_size=2))
+ #hidden layers
+model.add(LSTM(10))
+# model.add(Flatten())
+model.add(Dropout(0.6))
+model.add(Dense(1200, activation='relu',W_constraint=maxnorm(1)))
+# model.add(Dropout(0.6))
+model.add(Dense(500, activation='relu',W_constraint=maxnorm(1)))
 
+# model.add(Dropout(0.5))
  #output layer
 model.add(Dense(5, activation='softmax'))
 
@@ -130,54 +144,48 @@ model.add(Dense(5, activation='softmax'))
 model.summary()
 
 
+learning_rate=0.0001
+epochs = 60
+batch_size = 32 #32
+sgd = SGD(lr=learning_rate, nesterov=True, momentum=0.7, decay=1e-4)
+Nadam = keras.optimizers.Nadam(lr=0.002, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
+model.compile(loss='categorical_crossentropy', optimizer=Nadam, metrics=['accuracy'])
 
+
+tensorboard = keras.callbacks.TensorBoard(log_dir='./logs/log_25', histogram_freq=0, write_graph=True, write_images=False)
+checkpointer = ModelCheckpoint(filepath="./weights/weights_25.hdf5", verbose=1, save_best_only=True, monitor="val_loss")
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=0, verbose=1, mode='auto', cooldown=0, min_lr=1e-6)
+earlyStopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=6, verbose=1)
 
 # Loading best weights
-model.load_weights("./weights/weights_8.hdf5")
-
-
-
-
-learning_rate=0.0001
-epochs = 100
-batch_size = 128 #32
-sgd = SGD(lr=learning_rate, nesterov=True, momentum=0.7, decay=1e-4)
-model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-
-tensorboard = keras.callbacks.TensorBoard(log_dir='./logs/log_8', histogram_freq=0, write_graph=True, write_images=False)
-checkpointer = ModelCheckpoint(filepath="./weights/weights_8.hdf5", verbose=1, save_best_only=True, monitor="val_loss")
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8, patience=0, verbose=1, mode='auto', cooldown=0, min_lr=1e-6)
-
+# model.load_weights("./weights/weights_19.hdf5")
 
 print ("=============================== Training =========================================")
 
 # # uncommit this to train
 # # tensorboard --logdir=./logs
 
-# history  = model.fit(X_Train_encodedPadded_words, Y_train, epochs = epochs, batch_size=batch_size, verbose=1,
-#                     validation_data=(X_Val_encodedPadded_words, Y_Val), callbacks=[tensorboard, reduce_lr,checkpointer])
+history  = model.fit(X_Train_encodedPadded_words, Y_train, epochs = epochs, batch_size=batch_size, verbose=1,
+                    validation_data=(X_Val_encodedPadded_words, Y_Val), callbacks=[tensorboard, reduce_lr,checkpointer,earlyStopping])
 
 print ("=============================== Score =========================================")
 
-# scores = model.evaluate(X_Val_encodedPadded_words, Y_Val, verbose=0)
-# print("Accuracy: %.2f%%" % (scores[1]*100))
+scores = model.evaluate(X_Val_encodedPadded_words, Y_Val, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 print ("=============================== Predicting =========================================")
 
 f = open('Submission.csv', 'w')
 f.write('PhraseId,Sentiment\n')
 
 
-
-
 # predictions = model.predict(X_test_encodedPadded_words)
 predicted_classes = model.predict_classes(X_test_encodedPadded_words, batch_size=batch_size, verbose=1)
+# print np.sum(predicted_classes==Y_Val2)/(1.0*Y_Val2.shape[0])
 # print predicted_classes
 # preds = new_model.predict(x)
-
+# print predicted_classes
 for i in range(0,X_test_PhraseID.shape[0]):
     # pred =np.argmax(predictions[i])
-    # f.write(str(X_test_PhraseID[i])+","+str(pred)+'\n')
     f.write(str(X_test_PhraseID[i])+","+str(predicted_classes[i])+'\n')
     # print predictions[i],"=>",pred
 
